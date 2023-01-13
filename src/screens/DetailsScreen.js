056440dd4879513694070a5152b5dc1018e4f0dd
“Afsana <afsanaairin07@gmail.com>
2023-01-13 16:05:16 +0600

    Introduction to Instacity

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { SafeAreaView,StyleSheet, Text, View, ScrollView,Button } from 'react-native';
+
+// const DetailsScreen = () => {
+//     return (
+//       <View style={styles.container2}>
+//         <Text>Details Screen</Text>
+//       </View>
+//     );
+//   };
+
+// const styles = StyleSheet.create({
+// container2: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+// },
+// });
+
+// export default DetailsScreen;
---
+++ src/screens/ExploreScreen.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import { SafeAreaView,StyleSheet, Text, View, ScrollView,Button } from 'react-native';
+
+// const ExploreScreen = () => {
+//     return (
+//       <View style={styles.container3}>
+//         <Text>exploreScreen</Text>
+//         <Button title='Click Here' 
+//                 onPress={() => alert("Button clicked")}
+//         />
+//       </View>
+//     );
+//   };
+
+//   const styles = StyleSheet.create({
+//     container3: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+// });
+
+// export default ExploreScreen;
